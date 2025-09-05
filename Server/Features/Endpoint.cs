6f9b5049a8d3e1a5d7c5d5b3f1d39e2c09c290bc
Denny09310 <k.denny2000@gmail.com>
2025-09-05 10:24:23 +0200

    initial commit

---
+++ Server/Features/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Server.Features;
+
+[HttpGet("/hello")]
+[AllowAnonymous]
+public class Endpoint : EndpointWithoutRequest<Response>
+{
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        Response.Message = "Hello world!";
+        return Send.OkAsync(Response, ct);
+    }
+}
+
+public sealed class Response
+{
+    public string Message { get; set; }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+using Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddFastEndpoints(options =>
+{
+    options.SourceGeneratorDiscoveredTypes.AddRange(DiscoveredTypes.All);
+});
+builder.Services.SwaggerDocument();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseSwaggerGen();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.MapFastEndpoints(options =>
+{
+    options.Endpoints.RoutePrefix = "api";
+});
+app.MapFallbackToFile("index.html");
+
+app.Run();
