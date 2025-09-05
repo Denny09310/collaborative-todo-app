83c00bc24c060a3f1975b785fb0276f91df6eb7f
Denny09310 <k.denny2000@gmail.com>
2025-09-05 11:02:23 +0200

    feat: setup tailwindcss, setup preline

---
+++ Client/wwwroot/lib/Client.lib.module.js
@@ -0,0 +1,26 @@
+﻿import "./preline/preline.min.js";
+
+function autoInit() {
+    if (window.HSStaticMethods && typeof window.HSStaticMethods.autoInit === 'function') {
+        window.HSStaticMethods.autoInit();
+    }
+}
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), timeout);
+    };
+}
+
+const debouncedAutoInit = debounce(autoInit, 200);
+
+const observer = new MutationObserver(() => {
+    debouncedAutoInit();
+});
+
+observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+})
